cfd09bbd41942edb73a8aa42a3bf2f97cf815518
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-13 22:27:45 +0430

    modify the recipe_food.rb to add associations for food and recipe classes

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,4 @@
 class Food < ApplicationRecord
   belongs_to :user
-  # has_many :recipes through: :recipe_food
+  has_many :recipe_foods
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_13_112253) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_13_172309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_112253) do
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
+  create_table "recipe_foods", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.time "preparation_time"
+    t.time "cooking_time"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -32,4 +48,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_13_112253) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipes", "users"
 end
