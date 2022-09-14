e28ec71229dd6e42ab288eaf6c03f9356dc1e096
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-14 23:45:56 +0430

    added the public methode for recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipesController < ApplicationController
   # GET /recipes or /recipes.json
   def index
     @user = current_user
-    @recipes = @user.recipes.includes(:user).order(created_at: :desc)
+    @recipes = current_user.recipes.includes(:user).order(created_at: :desc)
   end
 
   # GET /recipes/1 or /recipes/1.json
@@ -15,6 +15,13 @@ class RecipesController < ApplicationController
   # GET /recipes/new
   def new
     @recipe = Recipe.new
+  end
+
+  def public
+    @public_recipes = Recipe.includes([:user], [:recipe_foods]).where(public: true).order(created_at: :desc)
+    @public_recipes.each do |recipe|
+      recipe.recipe_foods.all.includes([:food]).sort_by { |recipe_food| recipe_food.food.name }
+    end
   end
 
   # GET /recipes/1/edit
