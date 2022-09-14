e258b403adce2a916403af2c4f650f0a212b9c75
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-14 23:27:50 +0430

    solving linters

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,16 +1,15 @@
 def index
-    @food_amount = 0
-    @total_price = 0
-    @recipe_foods = []
-    @foods = current_user.foods
-    @recipes = current_user.recipes
-    @recipes.includes(:recipe_foods).each do |recipe|
-      recipe.recipe_foods.map do |food|
-        @recipe_foods << food
-      end
+  @food_amount = 0
+  @total_price = 0
+  @recipe_foods = []
+  @foods = current_user.foods
+  @recipes = current_user.recipes
+  @recipes.includes(:recipe_foods).each do |recipe|
+    recipe.recipe_foods.map do |food|
+      @recipe_foods << food
     end
-    @recipe_foods.each do |f|
-      @total_price += f.food.price * f.quantity
-    end
+  end
+  @recipe_foods.each do |f|
+    @total_price += f.food.price * f.quantity
   end
 end
