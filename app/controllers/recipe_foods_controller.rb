4ac2eaa7138744df518733f27020e8190fd42dd6
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-15 12:24:41 +0430

    solve the recipe paths for recipes controller

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -24,7 +24,7 @@ class RecipeFoodsController < ApplicationController
 
     respond_to do |format|
       if @recipe_food.save
-        format.html { redirect_to recipe_path(@recipe), notice: 'Recipe food was successfully created.' }
+        format.html { redirect_to user_recipe_path(@recipe, :user_id params[:user_id]), notice: 'Recipe food was successfully created. ✅' }
         format.json { render :show, status: :created, location: @recipe_food }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -37,7 +37,7 @@ class RecipeFoodsController < ApplicationController
   def update
     respond_to do |format|
       if @recipe_food.update(recipe_food_params)
-        format.html { redirect_to recipe_food_url(@recipe_food), notice: 'Recipe food was successfully updated.' }
+        format.html { redirect_to user_recipe_food_url(@recipe_food), notice: 'Recipe food was successfully updated. ✅' }
         format.json { render :show, status: :ok, location: @recipe_food }
       else
         format.html { render :edit, status: :unprocessable_entity }
