b03141956d553afcbe2a69f6614daa950c3d45cc
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-16 11:54:11 +0430

    write some tests for recipe_foods model

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe RecipeFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(name: 'John Doe', email: 'johndoe@gmail.com', password: 123_456, password_confirmation: 123_456)
+    @recipe = Recipe.create(name: 'Apples', preparation_time: '1 hour', cooking_time: '30 minutes',
+                            description: 'Lorem ipsum', public: false, user_id: @user.id)
+    @food = Food.create(name: 'Apples', measurement_unit: 'pieces', price: 18, quantity: 24, user_id: @user.id)
+  end
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      recipe = RecipeFood.new(recipe_id: @recipe.id, food_id: @food.id, quantity: 2)
+      expect(recipe).to be_valid
+    end
+
+    it 'is valid without a quantity' do
+      recipe = RecipeFood.new(recipe_id: @recipe.id, food_id: @food.id, quantity: nil)
+      expect(recipe).to_not be_valid
+    end
+
+    it 'is valid without a recipe_id' do
+      recipe = RecipeFood.new(recipe_id: nil, food_id: @food.id, quantity: 2)
+      expect(recipe).to_not be_valid
+    end
+
+    it 'is valid without a food_id' do
+      recipe = RecipeFood.new(recipe_id: @recipe.id, food_id: nil, quantity: 2)
+      expect(recipe).to_not be_valid
+    end
+  end
+
+  context 'associations' do
+    it 'belongs_to recipe' do
+      t = RecipeFood.reflect_on_association(:recipe)
+      expect(t.macro).to eq :belongs_to
+    end
+
+    it 'belongs_to food' do
+      t = RecipeFood.reflect_on_association(:food)
+      expect(t.macro).to eq :belongs_to
+    end
+  end
 end
