07767a9d11b06064bbeed5743f09f31514386e6d
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-09-16 11:29:18 +0430

    add validations for food model class

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,5 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :recipes, through: :recipe_foods
+  validates :name, :price, :quantity, :measurement_unit, presence: true
 end
